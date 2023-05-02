fdc45ec67a33e59db7e87702f7dc8369e4b63229
VassilevEmil <emilvassilev5@abv.bg>
2023-05-02 13:50:59 +0200

    played a little furhter

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import * as cheerio from "cheerio";
 
 import fetch from "node-fetch";
 
+// scraping f1 drivers
+
 async function getFormulaOneDrivers() {
   try {
     const response = await fetch("https://www.formula1.com/en/drivers.html");
@@ -38,4 +40,53 @@ async function getFormulaOneDrivers() {
   }
 }
 
-getFormulaOneDrivers();
+// Scraping blockchain news from Denmark
+
+async function getNewsFromDenmark() {
+  try {
+    const response = await fetch("https://cointelegraph.com/tags/denmark");
+    const body = await response.text();
+
+    // console.log(body);
+
+    const $ = cheerio.load(body);
+
+    // const items = [];
+    // $(".tag-page > .container ").map((i, el) => {
+    //   const test = $(el).find(".posts-listing__list").text();
+    //   const title = $(el).find(".posts-listing__list article:first").text();
+    //   const text = $(el).find(".post-card-inline__header").text();
+
+    //   console.log(text);
+    //   items.push({
+    //     title,
+    //   });
+    // });
+
+    const headers = [];
+
+    $(".tag-page > .container ").each((i, el) => {
+      $(el)
+        .find(".posts-listing__list article")
+        .each((j, article) => {
+          const title = $(article)
+            .find(".post-card-inline__header")
+            .text()
+            .trim();
+          const text = $(el).find(".post-card-inline__text").text();
+          headers.push({
+            title,
+            text,
+          });
+        });
+
+      console.log(headers);
+    });
+
+    // console.log(items);
+  } catch (error) {
+    console.log(error);
+  }
+}
+// getFormulaOneDrivers();
+getNewsFromDenmark();
